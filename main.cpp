bb97b089306edc6e349839d06380feeac4ce2059
Adrian McMenamin <adrianmcmenamin@gmail.com>
2013-11-04 23:03:30 +0000

    starting up

---
+++ main.cpp
@@ -0,0 +1,62 @@
+// JALAN - Just Another Lackey ANalyser
+// Copyright Adrian McMenamin <acm538@york.ac.uk>, 2013
+// Licensed under GNU GPL version 3
+//
+// Processes XMLised output from Valgrind's Lackey tool
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <fstream>
+#include <xercesc/util/PlatformUtils.hpp>
+
+
+using namespace std;
+using namespace xercesc;
+
+
+void usage()
+{
+	cout << "JALAN - Just Another Lackey ANalyser" << endl;
+	cout << "Copyright Adrian McMenamin, acm538@york.ac.uk, 2013" << endl;
+	cout << "Licensed under GNU GPL version 3." << endl;
+	cout << "Usage..." << endl;
+	cout << "jalan [options] inputfile" << endl;
+	cout << "Options..." << endl;
+	cout << "-f	Specify input file name" << endl;
+	cout << "-?	Print this usage message" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+
+	string xmlFile;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-f") == 0)
+		{
+			if (argc >= i + 1) {
+				xmlFile = argv[i + 1];
+				i++;
+				continue;
+			} else {
+				usage();
+				return 1;
+			}
+		}
+
+		if (strcmp(argv[i], "-?") == 0) {
+			usage();
+			return 0;
+		}
+	}
+	try {
+		XMPlatformUtils::Initialize();
+	}
+	catch (const XMLException& toCatch) {
+		cout << "Failed to initialise Xerces XML engine." << endl
+		return 1;
+	}
+
+	XMLPlatformUtils::Terminate();
+	return 0;
+}		
